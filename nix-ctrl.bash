d576eb46c77af90fb31788e486f635d9507b3026
Jay <95102557+ThizIsJay@users.noreply.github.com>
2021-12-26 22:27:33 +0100

    Vergeten oude te verwijderen

--- nix-ctrl.bash
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-case $1 in
-	switch) sudo nixos-rebuild switch --flake '.#';;
-	apply) sudo nixos-rebuild switch --flake '.#';;
-	test) sudo nixos-rebuild dry-run --flake '.#';;
-esac
-
