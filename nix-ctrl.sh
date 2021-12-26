919dadc095cb89b3b281cbb83a1cb63b7ef13be0
Jay Verrijt <acc.jverrijt@hotmail.com>
2021-12-26 22:30:32 +0100

    CH: Locatie fix in nix-ctrl

--- nix-ctrl.sh
+++ nix-ctrl.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 case $1 in
-	switch) sudo nixos-rebuild switch --flake '.#';;
-	apply) sudo nixos-rebuild switch --flake '.#';;
+	switch) sudo nixos-rebuild switch -I nixos-config=./configuration.nix --flake '.#';;
+	apply) sudo nixos-rebuild switch -I nixos-config=./configuration.nix --flake '.#';;
 	test) sudo nixos-rebuild dry-run --flake '.#';;
 esac
 
