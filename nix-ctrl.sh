9b6dbebe812cfe08e9c77ee59ffd5b8f9d705e68
Jay Verrijt <acc.jverrijt@gmail.com>
2021-12-29 13:47:21 +0000

    ADD: Nix-Ctrl.sh

--- nix-ctrl.sh
+++ nix-ctrl.sh
@@ -1,8 +1,15 @@
 #! /usr/bin/env bash
 
-case $1 in
-	switch) sudo nixos-rebuild switch -I nixos-config=./configuration.nix --flake '.#';;
-	apply) sudo nixos-rebuild switch -I nixos-config=./configuration.nix --flake '.#';;
-	test) sudo nixos-rebuild dry-run --flake '.#';;
+case $1 in 
+	apply-sys)
+		pushd ~/.GitHub/NixOS-Unstable/
+		sudo nixos-rebuild switch -I nixos-config=./configuration.nix ;;
+	apply-user)
+		pushd ~/.GitHub/NixOS-Unstable/
+		home-manager switch -f ./sys/users/homemanager.nix
+		;;
+	update-sys)
+		sudo nix-channel --update ;;
+	update-user)
+		nix-channel --update ;;
 esac
-
